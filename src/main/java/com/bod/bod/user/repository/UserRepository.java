57600d2ab17564463652d1599537faec93190b65
kinam <maltize9292@gmail.com>
2024-07-23 20:12:48 +0900

    feat / 어드민 유저 전체조회 기능구현

--- src/main/java/com/bod/bod/user/repository/UserRepository.java
+++ src/main/java/com/bod/bod/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.bod.bod.user.repository;
 
 import com.bod.bod.user.entity.User;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -9,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);
 
 	Optional<User> findByEmail(String email);
+
+	Page<User> findAll(Pageable pageable);
 }
