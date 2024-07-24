a3f6a49d96401bb6f5ebf542a87ff5f787fe24bf
Hyungs0703 <hyungs0703@naver.com>
2024-07-24 21:24:30 +0900

    소셜로그인 기능구현

--- src/main/java/com/bod/bod/user/oauth2/OAuth2UserInfo.java
+++ src/main/java/com/bod/bod/user/oauth2/OAuth2UserInfo.java
@@ -1,14 +1,20 @@
 package com.bod.bod.user.oauth2;
 
 import com.bod.bod.user.entity.UserRole;
+import com.bod.bod.user.entity.UserStatus;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class OAuth2UserInfo {
-	private final UserRole role;
+
 	private final String name;
 	private final String username;
+	private final String password;
+	private final String nickname;
 	private final String email;
+	private final String profileImage;
+	private final UserRole role;
+	private final UserStatus userStatus;
 }
