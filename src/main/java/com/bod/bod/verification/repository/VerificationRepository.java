ec455ad1662274d54ceac2ad4fc52b73edf373cb
haejungHan <tinley705@gmail.com>
2024-07-25 16:51:30 +0900

    🩹 fix : 일1회 챌린지 인증 구현

--- src/main/java/com/bod/bod/verification/repository/VerificationRepository.java
+++ src/main/java/com/bod/bod/verification/repository/VerificationRepository.java
@@ -2,7 +2,10 @@ package com.bod.bod.verification.repository;
 
 import com.bod.bod.global.exception.ErrorCode;
 import com.bod.bod.global.exception.GlobalException;
+import com.bod.bod.user.entity.User;
 import com.bod.bod.verification.entity.Verification;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,8 +14,12 @@ public interface VerificationRepository extends JpaRepository<Verification, Long
 
   default Verification findVerificationById(Long verificationId) {
 	return findById(verificationId).orElseThrow(
-		()-> new GlobalException(ErrorCode.NOT_FOUND_VERIFICATION));
+		() -> new GlobalException(ErrorCode.NOT_FOUND_VERIFICATION));
   }
 
-    Page<Verification> findByChallengeId(Long challengeId, Pageable pageable);
+  Page<Verification> findByChallengeId(Long challengeId, Pageable pageable);
+
+  List<Verification> findByCreatedAtBetweenAndUser(LocalDateTime startDateTime, LocalDateTime endDateTime, User user);
 }
+
+
