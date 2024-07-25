b136e514e58907c8dbcf24ccb6990086927ca241
W.llama <tmdqja96@gmail.com>
2024-07-25 16:54:22 +0900

    UserChallenge 중간테이블 생성

---
+++ src/main/java/com/bod/bod/challenge/entity/UserChallenge.java
@@ -0,0 +1,32 @@
+package com.bod.bod.challenge.entity;
+
+import com.bod.bod.global.TimeStamp;
+import com.bod.bod.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "db_userChallenges")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserChallenge extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+}
