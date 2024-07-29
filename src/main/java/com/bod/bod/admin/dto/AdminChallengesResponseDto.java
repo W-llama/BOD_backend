31f0a34b4e4f0bab565a99b67558eb1893abc475
kinam <maltize9292@gmail.com>
2024-07-29 19:53:56 +0900

    feat / 어드민 챌린지 전체 조회 기능 구현

---
+++ src/main/java/com/bod/bod/admin/dto/AdminChallengesResponseDto.java
@@ -0,0 +1,37 @@
+package com.bod.bod.admin.dto;
+
+import com.bod.bod.challenge.entity.Category;
+import com.bod.bod.challenge.entity.Challenge;
+import com.bod.bod.challenge.entity.ConditionStatus;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AdminChallengesResponseDto {
+
+    private Long challengeId;
+
+    private String title;
+
+    private Category category;
+
+    private ConditionStatus conditionStatus;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    public AdminChallengesResponseDto(Challenge challenge) {
+        this.challengeId = challenge.getId();
+        this.title = challenge.getTitle();
+        this.category = challenge.getCategory();
+        this.conditionStatus = challenge.getConditionStatus();
+        this.startTime = challenge.getStartTime();
+        this.endTime = challenge.getEndTime();
+    }
+
+
+
+}
