e0dffad6475b50a1742ca5f39062ba3ec6284cbe
Hyungs0703 <hyungs0703@naver.com>
2024-08-02 13:48:18 +0900

    인증 관련 로직수적

--- src/main/java/com/bod/bod/verification/dto/VerificationWithChallengeResponseDto.java
+++ src/main/java/com/bod/bod/verification/dto/VerificationWithChallengeResponseDto.java
@@ -10,16 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class VerificationWithChallengeResponseDto {
 
-  private Long verificationId;
-  private String challengeTitle;
-  private Status status;
-  private LocalDateTime createdAt;
+	private Long verificationId;
+	private String challengeTitle;
+	private Status status;
+	private String verificationImageUrl;
+	private LocalDateTime createdAt;
 
-  public VerificationWithChallengeResponseDto(Long verificationId, String challengeTitle, Status status, LocalDateTime createdAt) {
-	this.verificationId = verificationId;
-	this.challengeTitle = challengeTitle;
-	this.status = status;
-	this.createdAt = createdAt;
-  }
+	public VerificationWithChallengeResponseDto(Long verificationId, String challengeTitle, Status status, String verificationImageUrl, LocalDateTime createdAt) {
+		this.verificationId = verificationId;
+		this.challengeTitle = challengeTitle;
+		this.status = status;
+		this.verificationImageUrl = verificationImageUrl;
+		this.createdAt = createdAt;
+	}
 
 }
