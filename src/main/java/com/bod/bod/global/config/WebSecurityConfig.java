fe64ee566f8b57f5d37848147582f41926183f7e
Hyungs0703 <hyungs0703@naver.com>
2024-08-09 21:03:11 +0900

    WebSecurityConfig 변경

--- src/main/java/com/bod/bod/global/config/WebSecurityConfig.java
+++ src/main/java/com/bod/bod/global/config/WebSecurityConfig.java
@@ -80,8 +80,7 @@ public class WebSecurityConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-//					.allowedOrigins("http://3.37.71.106", "http://challengersbod.store")
-					.allowedOrigins("http://localhost", "http://frontend")
+					.allowedOrigins("http://challengersbod.store")
 					.exposedHeaders("authorization") // 이 부분을 추가합니다.
 					.allowCredentials(true) // 쿠키 인증 요청 허용
 					.allowedHeaders("*")
